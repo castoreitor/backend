dc744b45faf72114f78e685eab051af35120b800
OEBO <obenitezoidor@gmail.com>
2025-03-15 23:55:03 -0500

    correcciones

--- src/model/db.js
+++ src/model/db.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 // Verifica que las variables de entorno se carguen bien
-console.log("Usando DB_HOST:", process.env.DB_HOST);
-console.log("Usando DB_PORT:", process.env.DB_PORT);
+console.log("Usando DB_HOST:", process.env.MYSQL_HOST);
+console.log("Usando DB_PORT:", process.env.MYSQL_PORT);
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_NAME,
+  port: process.env.DMYSQL_PORT || 3306,
   ssl: { rejectUnauthorized: false }, // 👈 Desactiva la validación del certificado
 });
 
